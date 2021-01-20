1adbef6357c8efc7f19e0e02d72d2a25ce90124c
Javier Lete <javier@eskura21.com>
2021-01-20 10:50:36 +0100

    Inicio holamundocomplejospring

---
+++ holamundocomplejospring/src/main/java/com/ipartek/formacion/spring/holamundocomplejospring/HolamundocomplejospringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.holamundocomplejospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundocomplejospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundocomplejospringApplication.class, args);
+	}
+
+}
