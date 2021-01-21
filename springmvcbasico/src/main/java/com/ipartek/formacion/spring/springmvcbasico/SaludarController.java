a41c555607233269a72dbd1155388f2053a7da54
Javier Lete <javier@eskura21.com>
2021-01-21 09:39:10 +0100

    Saludar versión inicial con parámetro obligatorio

---
+++ springmvcbasico/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SaludarController.java
@@ -0,0 +1,15 @@
+package com.ipartek.formacion.spring.springmvcbasico;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SaludarController {
+	@GetMapping("/saludo")
+	public String saludo(@RequestParam String nombre, Model modelo) {
+		modelo.addAttribute("nombre", nombre);
+		return "saludo";
+	}
+}
