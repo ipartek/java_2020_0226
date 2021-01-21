d8f06ae322f1e62737c29b1f06cf50c5de66132e
Javier Lete <javier@eskura21.com>
2021-01-21 09:24:27 +0100

    Inicio springmvcbasico

---
+++ springmvcbasico/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SpringmvcbasicoApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvcbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcbasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcbasicoApplication.class, args);
+	}
+
+}
---
+++ springmvcbasico/src/test/java/com/ipartek/formacion/spring/springmvcbasico/SpringmvcbasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvcbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcbasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
