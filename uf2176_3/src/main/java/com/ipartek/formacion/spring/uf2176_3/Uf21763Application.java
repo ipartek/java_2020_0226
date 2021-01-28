5ce0d5f6811adcfd973c7368f8155bd9f6650df3
Javier Lete <javier@eskura21.com>
2021-01-28 08:25:15 +0100

    Inicio uf2176_3

---
+++ uf2176_3/src/main/java/com/ipartek/formacion/spring/uf2176_3/Uf21763Application.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.uf2176_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uf21763Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uf21763Application.class, args);
+	}
+
+}
---
+++ uf2176_3/src/test/java/com/ipartek/formacion/spring/uf2176_3/Uf21763ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.uf2176_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf21763ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
