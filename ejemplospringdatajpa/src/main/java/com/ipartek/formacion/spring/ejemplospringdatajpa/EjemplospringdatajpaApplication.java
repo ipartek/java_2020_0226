8d85a25e06ec31818365cb4d284dcde216c837ca
Javier Lete <javier@eskura21.com>
2021-02-02 08:16:52 +0100

    Inicio SpringDataJPA

---
+++ ejemplospringdatajpa/src/main/java/com/ipartek/formacion/spring/ejemplospringdatajpa/EjemplospringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ejemplospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplospringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringdatajpaApplication.class, args);
+	}
+
+}
---
+++ ejemplospringdatajpa/src/test/java/com/ipartek/formacion/spring/ejemplospringdatajpa/EjemplospringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.ejemplospringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplospringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
