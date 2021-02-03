5f9229c6c97f222e4633b6df309dc65f07b07176
Javier Lete <javier@eskura21.com>
2021-02-03 13:46:55 +0100

    Inicio servidorrestspringdatajpa

---
+++ servidorrestspringdatajpa/src/main/java/com/ipartek/formacion/spring/servidorrestspringdatajpa/ServidorrestspringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.servidorrestspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorrestspringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorrestspringdatajpaApplication.class, args);
+	}
+
+}
---
+++ servidorrestspringdatajpa/src/test/java/com/ipartek/formacion/spring/servidorrestspringdatajpa/ServidorrestspringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.servidorrestspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorrestspringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
