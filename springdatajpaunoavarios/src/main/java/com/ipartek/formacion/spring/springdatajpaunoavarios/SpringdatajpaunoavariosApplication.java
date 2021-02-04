f10f8a9bd9bf37f6c6821ae217ed52cc10e348e4
Javier Lete <javier@eskura21.com>
2021-02-04 08:17:53 +0100

    Inicio springdatajpaunoavarios

---
+++ springdatajpaunoavarios/src/main/java/com/ipartek/formacion/spring/springdatajpaunoavarios/SpringdatajpaunoavariosApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springdatajpaunoavarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaunoavariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaunoavariosApplication.class, args);
+	}
+
+}
---
+++ springdatajpaunoavarios/src/test/java/com/ipartek/formacion/spring/springdatajpaunoavarios/SpringdatajpaunoavariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springdatajpaunoavarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaunoavariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
