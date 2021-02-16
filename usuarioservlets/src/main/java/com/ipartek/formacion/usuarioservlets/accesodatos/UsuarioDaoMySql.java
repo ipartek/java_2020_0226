7f2142e916cfa138d7f8b668ac3aac91ba001ea8
Javier Lete <javier@eskura21.com>
2021-02-16 09:14:15 +0100

    Implementación del DAO para MySQL

---
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/accesodatos/UsuarioDaoMySql.java
@@ -0,0 +1,86 @@
+package com.ipartek.formacion.usuarioservlets.accesodatos;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.ipartek.formacion.usuarioservlets.entidades.Rol;
+import com.ipartek.formacion.usuarioservlets.entidades.Usuario;
+
+public class UsuarioDaoMySql implements UsuarioDao {
+
+	private static final String SQL_SELECT = "SELECT u.id u_id, email, password, r.id r_id, nombre r_nombre, descripcion r_descripcion FROM usuarios u JOIN roles r ON u.roles_id = r.id";
+	private DataSource dataSource = null;
+	
+	public UsuarioDaoMySql() {
+		try {
+			InitialContext initCtx=new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			dataSource = (DataSource)envCtx.lookup("jdbc/usuarioservlets");
+		} catch (NamingException e) {
+			throw new AccesoDatosException("No se ha encontrado el JNDI de usuarioservlets", e);
+		}
+	}
+	
+	@Override
+	public Iterable<Usuario> obtenerTodos() {
+		try (Connection con = dataSource.getConnection();
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(SQL_SELECT)) {
+			ArrayList<Usuario> usuarios = new ArrayList<>();
+			Usuario usuario;
+			Rol rol;
+			
+			while(rs.next()) {
+				rol = new Rol(rs.getLong("r_id"), rs.getString("r_nombre"), rs.getString("r_descripcion"));
+				usuario = new Usuario(rs.getLong("u_id"), rs.getString("email"), rs.getString("password"), rol);
+				
+				usuarios.add(usuario);
+			}
+			
+			return usuarios;
+		} catch (SQLException e) {
+			throw new AccesoDatosException("No se han podido obtener todos los registros de usuarios");
+		} catch (Exception e) {
+			throw new AccesoDatosException("ERROR NO ESPERADO: No se han podido obtener todos los registros de usuarios");
+		}
+	}
+
+	@Override
+	public Usuario obtenerPorId(Long id) {
+		// TODO Auto-generated method stub
+		return UsuarioDao.super.obtenerPorId(id);
+	}
+
+	@Override
+	public Usuario insertar(Usuario t) {
+		// TODO Auto-generated method stub
+		return UsuarioDao.super.insertar(t);
+	}
+
+	@Override
+	public Usuario modificar(Usuario t) {
+		// TODO Auto-generated method stub
+		return UsuarioDao.super.modificar(t);
+	}
+
+	@Override
+	public void borrar(Long id) {
+		// TODO Auto-generated method stub
+		UsuarioDao.super.borrar(id);
+	}
+
+	@Override
+	public Usuario obtenerPorEmail(String email) {
+		// TODO Auto-generated method stub
+		return UsuarioDao.super.obtenerPorEmail(email);
+	}
+	
+}
