b6f720f1c9e8491d5ac2a66a088e5d9ce28dd552
Javier Lete <javier@eskura21.com>
2021-02-17 10:10:19 +0100

    Filtro de acceso a administración al rol ADMIN

---
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/filtros/AdminFilter.java
@@ -0,0 +1,41 @@
+package com.ipartek.formacion.usuarioservlets.filtros;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.ipartek.formacion.usuarioservlets.entidades.Usuario;
+
+@WebFilter("/admin/*")
+public class AdminFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+			
+		if(usuario != null && usuario.getRol() != null && "ADMIN".equals(usuario.getRol().getNombre())) {
+			chain.doFilter(request, response);
+		} else {
+			request.setAttribute("mensaje", "Se necesita un usuario con privilegios administrativos para poder entrar en la zona de administración");
+			request.setAttribute("nivel", "danger");
+			
+			req.getRequestDispatcher("/login").forward(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+}
