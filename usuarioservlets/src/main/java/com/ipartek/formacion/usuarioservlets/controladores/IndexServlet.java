2bb93f3dceabe0868c1765e131711d39b1cf97db
Javier Lete <javier@eskura21.com>
2021-02-18 10:12:37 +0100

    (Añadir @Override)

--- usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/controladores/IndexServlet.java
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/controladores/IndexServlet.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/vistas/index.jsp").forward(request, response);
 	}
-
+	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
