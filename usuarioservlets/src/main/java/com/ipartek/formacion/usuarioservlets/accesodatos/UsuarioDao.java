28bbfb742cb41a7f300e73e28d5a3393466a7131
Javier Lete <javier@eskura21.com>
2021-02-22 10:55:43 +0100

    Nuevo requisito para el DAO de usuario: obtenerPorIdRol

--- usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/accesodatos/UsuarioDao.java
+++ usuarioservlets/src/main/java/com/ipartek/formacion/usuarioservlets/accesodatos/UsuarioDao.java
@@ -6,4 +6,8 @@ public interface UsuarioDao extends Dao<Usuario> {
 	default Usuario obtenerPorEmail(String email) {
 		throw new AccesoDatosException("NO IMPLEMENTADO");
 	}
+	
+	default Iterable<Usuario> obtenerPorIdRol(Long id) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
 }
