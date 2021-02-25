8cd3caaa4d3a6c3c6feb7d51dcacd354ea2284ee
Javier Lete <javier@eskura21.com>
2021-02-25 09:46:12 +0100

    Inicio springsecurityejemplo

---
+++ springsecurityejemplo/src/main/java/com/ipartek/formacion/spring/springsecurityejemplo/SpringsecurityejemploApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springsecurityejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityejemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityejemploApplication.class, args);
+	}
+
+}
---
+++ springsecurityejemplo/src/test/java/com/ipartek/formacion/spring/springsecurityejemplo/SpringsecurityejemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springsecurityejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityejemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
