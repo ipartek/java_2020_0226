3a860cd773c1dc21a638f29c37e9f175c06d93c8
Javier Lete <javier@eskura21.com>
2021-02-25 08:14:58 +0100

    Inicio de springmvcjsp SIN Thymeleaf y con Spring Web y Dev Tools

---
+++ springmvcjsp/src/main/java/com/ipartek/formacion/spring/springmvcjsp/SpringmvcjspApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcjspApplication.class, args);
+	}
+
+}
