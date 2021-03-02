968dc14e690fdf2abfd869d02ea7dbdf4fa7ffee
Javier Lete <javier@eskura21.com>
2021-03-02 08:55:14 +0100

    Autenticación con base de datos MySQL

--- springsecurityejemplo/src/main/java/com/ipartek/formacion/spring/springsecurityejemplo/configuraciones/WebSecurityConfig.java
+++ springsecurityejemplo/src/main/java/com/ipartek/formacion/spring/springsecurityejemplo/configuraciones/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.ipartek.formacion.spring.springsecurityejemplo.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,17 +31,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.permitAll();
 	}
 
-	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user =
-				 User.withDefaultPasswordEncoder()
-					.username("user")
-					.password("password")
-					.roles("USER")
-					.build();
-
-			return new InMemoryUserDetailsManager(user);
+	// https://www.baeldung.com/spring-security-jdbc-authentication
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
+// https://www.browserling.com/tools/bcrypt
+	
+//	CREATE TABLE users (
+//			  username VARCHAR(50) NOT NULL,
+//			  password VARCHAR(100) NOT NULL,
+//			  enabled TINYINT NOT NULL DEFAULT 1,
+//			  PRIMARY KEY (username)
+//			);
+//			  
+//			CREATE TABLE authorities (
+//			  username VARCHAR(50) NOT NULL,
+//			  authority VARCHAR(50) NOT NULL,
+//			  FOREIGN KEY (username) REFERENCES users(username)
+//			);
+//
+//			CREATE UNIQUE INDEX ix_auth_username
+//			  on authorities (username,authority);
+//	-- User user/pass
+//	INSERT INTO users (username, password, enabled)
+//	  values ('user',
+//	    '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a',
+//	    1);
+//
+//	INSERT INTO authorities (username, authority)
+//	  values ('user', 'ROLE_USER');
 }
