dde8a33f5c0117a549343ffb5e5607d617b1b9e4
Javier Lete <javier@eskura21.com>
2021-03-08 10:41:14 +0100

    Controlador de alta de cliente

---
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/controladores/ClienteServlet.java
@@ -0,0 +1,42 @@
+package com.ipartek.formacion.ejemplofinal.controladores;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.ejemplofinal.entidades.Cliente;
+
+@WebServlet("/cliente")
+public class ClienteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher(Config.PATH_VISTAS + "cliente.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String nombre = request.getParameter("nombre");
+		String apellidos = request.getParameter("apellidos");
+		String cif = request.getParameter("cif");
+		String fechaNacimiento = request.getParameter("fecha-nacimiento");
+		
+		Cliente cliente = new Cliente(null, nombre, apellidos, cif, LocalDate.parse(fechaNacimiento), null);
+		
+		// TODO: Validar cliente
+		
+		Config.clienteNegocio.altaCliente(cliente);
+		
+		request.setAttribute("cliente", cliente);
+		request.getRequestDispatcher(Config.PATH_VISTAS + "factura").forward(request, response);
+	}
+
+}
--- ejemplofinal/src/com/ipartek/formacion/ejemplofinal/controladores/Config.java
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/controladores/Config.java
@@ -1,11 +1,13 @@
 package com.ipartek.formacion.ejemplofinal.controladores;
 
 import com.ipartek.formacion.ejemplofinal.logicanegocio.CarritoNegocio;
+import com.ipartek.formacion.ejemplofinal.logicanegocio.ClienteNegocio;
 import com.ipartek.formacion.ejemplofinal.logicanegocio.FabricaNegocio;
 
 public class Config {
 	private Config() {}
 	
 	static final String PATH_VISTAS = "/WEB-INF/vistas/";
-	static final CarritoNegocio carritoNegocio = FabricaNegocio.getCarritoNegocio();	
+	static final CarritoNegocio carritoNegocio = FabricaNegocio.getCarritoNegocio();
+	static final ClienteNegocio clienteNegocio = FabricaNegocio.getClienteNegocio();
 }
