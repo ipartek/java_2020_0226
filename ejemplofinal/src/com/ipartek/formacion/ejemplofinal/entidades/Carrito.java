a078522b86a24aca1f75f1690a2c9988752a364b
Javier Lete <javier@eskura21.com>
2021-03-15 08:56:42 +0100

    Ejemplo de documentación de JavaDoc

--- ejemplofinal/src/com/ipartek/formacion/ejemplofinal/entidades/Carrito.java
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/entidades/Carrito.java
@@ -3,12 +3,29 @@ package com.ipartek.formacion.ejemplofinal.entidades;
 import java.io.Serializable;
 import java.util.TreeMap;
 
+/**
+ * Representa el carrito de la compra de los usuarios de la aplicación
+ * 
+ * @author Javier Lete
+ * @version 1.0
+ */
 public class Carrito implements Serializable {
 
+	/**
+	 * Necesario para los elementos Serializables
+	 */
 	private static final long serialVersionUID = -370385928841361725L;
 
+	/**
+	 * Almacenamos las líneas de carrito que contendrán el producto y cantidad
+	 */
 	private TreeMap<Long, DetalleCarrito> lineas = new TreeMap<>();
 	
+	/**
+	 * Añade el producto en una cantidad concreta al carrito
+	 * @param producto producto a introducir en el carrito
+	 * @param cantidad cantidad del producto a introducir
+	 */
 	public void addProducto(Producto producto, Integer cantidad) {
 		Long id = producto.getId();
 		
@@ -25,6 +42,10 @@ public class Carrito implements Serializable {
 		}
 	}
 	
+	/**
+	 * Método para la obtención del contenido del carrito
+	 * @return líneas del carrito (producto, cantidad)
+	 */
 	public Iterable<DetalleCarrito> getLineas() {
 		return lineas.values();
 	}
