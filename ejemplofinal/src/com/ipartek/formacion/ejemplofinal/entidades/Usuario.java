8b249b733da36ad66c20e165b16d39414ca8c1ca
Javier Lete <javier@eskura21.com>
2021-03-15 09:17:28 +0100

    Usuario serializable

--- ejemplofinal/src/com/ipartek/formacion/ejemplofinal/entidades/Usuario.java
+++ ejemplofinal/src/com/ipartek/formacion/ejemplofinal/entidades/Usuario.java
@@ -1,11 +1,16 @@
 package com.ipartek.formacion.ejemplofinal.entidades;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor
-public class Usuario {
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 6810679589257920056L;
+	
 	private Long id;
 	private String email;
 	private String password;
