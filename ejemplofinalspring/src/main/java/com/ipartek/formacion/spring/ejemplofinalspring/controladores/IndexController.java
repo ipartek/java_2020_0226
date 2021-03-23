60423703a14f678bb43e4640473a5525f0d7c0a4
Javier Lete <javier@eskura21.com>
2021-03-23 12:11:37 +0100

    Formato de números y fechas

--- ejemplofinalspring/src/main/java/com/ipartek/formacion/spring/ejemplofinalspring/controladores/IndexController.java
+++ ejemplofinalspring/src/main/java/com/ipartek/formacion/spring/ejemplofinalspring/controladores/IndexController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.ipartek.formacion.spring.ejemplofinalspring.entidades.Carrito;
